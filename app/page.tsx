cf6b023eee92f2a5f5f872dfe081f7f11b13c23c
amroodh <tannedtheta@duck.com>
2026-06-04 11:17:25 +0530

    Fixed Missing Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { pf } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import {inter } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -17,7 +18,7 @@ export default function Page() {
         <div
   className={styles.shape} />
           <p
-      className={`${pf.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
       <strong>Welcome to Acme.</strong> This is the example for the{' '}
       <a href="https://nextjs.org/learn/" className="text-blue-500">
